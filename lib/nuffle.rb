e401c31d98e72ef33b4a51024fc1170f79043a52
Zachary Flower <zach@ninjaninja.net>
2016-05-20 13:46:41 -0600

    update nuffle to new dice calculator functionality

--- lib/nuffle.rb
+++ lib/nuffle.rb
@@ -1,11 +1,83 @@
 class Nuffle
 
-  # Roll an x-sided dice
-  def self.roll(x)
-    # validate
-    raise "Invalid number of sides." if !x.is_a?(Integer) || x < 2
+  def self.roll(x = '')
+    ::Nuffle::Calculator.new(x).calculate
+  end
+end
 
-    # roll
-    return 1 + rand(x);
+class Nuffle::Calculator
+
+  attr_reader :input,
+              :rolls,
+              :equation,
+              :result
+
+  def initialize(input)
+    validate(input)
+  end
+
+  def calculate
+    # reset the rolls array
+    @rolls = []
+
+    # throw rolls and replace 'xdy' dice notation with results
+    @equation = @input.gsub(/(\d+)d(\d+)/) do |match|
+      rolls = []
+
+      (1..$1.to_i).each do
+        rolls << 1 + rand($2.to_i)
+      end
+
+      # save individual roll results
+      @rolls << {
+          'notation' => match,
+          'rolls' => rolls
+        }
+
+      "(#{rolls.join(" + ")})"
+    end
+
+    # calculate result
+    @result = eval(@equation);
+
+    self
+  end
+
+  private
+
+  def validate(input)
+    # has to be something we can calculate
+    raise("Input must be an equation or a number.") unless input.is_a?(String) || input.is_a?(Numeric)
+
+    # no empty inputs
+    raise("Input can't be blank.") if input.to_s.strip == ''
+
+    # validate the input format
+    raise("Invalid equation.") unless /^[\(\s]*(([1-9][0-9]*d[1-9][0-9]*)|\d+)[\s\)]*(\s*([\-\+\*\/])[\s\(]*(([1-9][0-9]*d[1-9][0-9]*)|\d+)\)*)*$/i.match(input)
+
+    # make sure the parens are balanced
+    raise("Unbalanced parens.") unless is_balanced?(input)
+
+    @input = input
+  end
+
+  def is_balanced?(input)
+    balance = 0
+
+    input.split("").each do |char|
+      if char == '('
+        balance += 1
+      elsif char == ')'
+        balance -= 1
+      end
+
+      # found a close paren without a matching open paren,
+      # no need to continue further
+      if balance < 0
+        break
+      end
+    end
+
+    return balance === 0
   end
 end
